0e095e8aabba877aeea327720a3df2a6bb8031bf
Raúl Granados <raul.granados12@gmail.com>
2025-08-31 23:28:07 -0600

    Ultimo completado: 04_06_str_slice

--- exercises/04_traits/03_operator_overloading/src/lib.rs
+++ exercises/04_traits/03_operator_overloading/src/lib.rs
@@ -8,7 +8,15 @@ struct Ticket {
 
 // TODO: Implement the `PartialEq` trait for `Ticket`.
 
-impl PartialEq for Ticket {}
+impl PartialEq for Ticket {
+    fn eq(&self, other: &Self) -> bool {
+        if *self.title == other.title && *self.status == other.status && *self.description == other.description {
+            return true
+        }
+
+        false
+    }
+}
 
 #[cfg(test)]
 mod tests {
